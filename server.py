2c35ba3070a418b9b8cb8ec3804fa5b30225979e
muoshuei <llh03098@gmail.com>
2025-11-22 19:20:25 +0800

    refactor: refine mer confict leftovers

--- server.py
+++ server.py
@@ -5,17 +5,12 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 from config import line_bot_api, handler
-<<<<<<< HEAD
-from services.scraper import get_104_job_data
-=======
 from services.process_job_link import process_job_url
 from services.predict import FraudPredictor
 from services.ui_renderer import create_risk_flex_message
->>>>>>> hubert-refactor
 
 from services.ui_renderer import create_risk_flex_message
 from utils import download_multiple
-from predict.predict import FraudPredictor
 from typing import cast
 app = FastAPI(title="Job Scam Detector")
 
@@ -61,18 +56,13 @@ def handle_message(event):
         target_url = match.group(0)
         job_data = process_job_url(target_url)
         
-<<<<<<< HEAD
         #TODO threads
 
         if not job_data:
-=======
-        if job_data.empty:
->>>>>>> hubert-refactor
              reply = TextSendMessage(text="❌ 無法讀取職缺資料，請確認該職缺是否已下架。")
              line_bot_api.reply_message(event.reply_token, reply)
              return
 
-<<<<<<< HEAD
         # risk_result = analyze_risk(job_data)
         predictor = cast(FraudPredictor, app.state.predictor)
 
@@ -93,18 +83,6 @@ def handle_message(event):
         flex_message = create_risk_flex_message(risk_result)
 
         line_bot_api.reply_message(event.reply_token, flex_message)
-=======
-        predictor = FraudPredictor()
-        predict_risk = predictor.predict_csv(job_data)
-        reply_string = TextSendMessage(text="predict_risk")
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_string))
-        print("Prediction Result:")
-        print(predict_risk)
-        
-        # flex_message = create_risk_flex_message(predict_risk) # 還要重新設計
-        
-        # line_bot_api.reply_message(event.reply_token, flex_message)
->>>>>>> hubert-refactor
 
     else:
         # 選項 B: 引導使用者 (適合一對一)
